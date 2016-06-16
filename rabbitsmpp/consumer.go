3938efafb96efd411f990769ae227598f80acbd4
Cesar Rodriguez Villanueva <cesar.rdz.v@gmail.com>
2016-06-16 19:18:23 +0300

    Manage channel connection more cleanly

--- rabbitsmpp/consumer.go
+++ rabbitsmpp/consumer.go
@@ -2,32 +2,39 @@ package rabbitsmpp
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 )
 
 type Consumer interface {
 	Consume() (chan Job, error)
+	Stop() error
 	Client
 }
 
 type consumer struct {
 	*client
-	stop chan bool
+	stop    chan bool
+	channel Channel
 }
 
 func NewConsumer(conf Config) (Consumer, error) {
 	c := NewClient(conf).(*client)
 	stop := make(chan bool)
-	return &consumer{c, stop}, nil
+	return &consumer{c, stop, nil}, nil
 }
 
 func (c *consumer) Consume() (chan Job, error) {
+	if c.channel != nil {
+		return nil, errors.New("consumer already active")
+	}
 	ch, err := c.Channel()
 	if err != nil {
 		return nil, err
 	}
+	c.channel = ch
 
-	q, err := ch.QueueDeclare(
+	q, err := c.channel.QueueDeclare(
 		c.QueueName(), // name
 		true,          // durable
 		false,         // delete when unused
@@ -39,7 +46,7 @@ func (c *consumer) Consume() (chan Job, error) {
 		return nil, err
 	}
 
-	msgs, err := ch.Consume(
+	msgs, err := c.channel.Consume(
 		q.Name, // queue
 		"",     // consumer
 		false,  // auto-ackey
@@ -54,7 +61,6 @@ func (c *consumer) Consume() (chan Job, error) {
 	jobChan := make(chan Job)
 
 	go func() {
-		defer ch.Close()
 		for {
 			select {
 			case d := <-msgs:
@@ -76,7 +82,25 @@ func (c *consumer) Consume() (chan Job, error) {
 	return jobChan, nil
 }
 
-func (c *consumer) Close() error {
+func (c *consumer) sendStop() {
 	c.stop <- true
-	return c.client.Close()
+}
+
+func (c *consumer) Stop() error {
+	if c.channel == nil {
+		return nil
+	}
+	c.sendStop()
+	err := c.channel.Close()
+	c.channel = nil
+	return err
+}
+
+func (c *consumer) Close() error {
+	if c.channel == nil {
+		return nil
+	}
+	go c.sendStop()
+	c.channel = nil
+	return c.conn.Close()
 }
