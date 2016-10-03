ad2009f1858af1b602ebb586a33ef18650782029
makkalot <makkalot@gmail.com>
2016-10-03 12:40:00 +0100

    delayed publisher

--- rabbitsmpp/publisher.go
+++ rabbitsmpp/publisher.go
@@ -2,6 +2,7 @@ package rabbitsmpp
 
 import (
 	"encoding/json"
+	"strconv"
 
 	"github.com/streadway/amqp"
 )
@@ -57,6 +58,101 @@ func (p *publisher) Publish(j Job) error {
 			ContentType: "application/json",
 			Body:        bodyBytes,
 		})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type delayedPublisher struct {
+	*client
+	delayExchange string
+	delayMS       int
+}
+
+// NewDelayedPublisher works the same way as the normal publisher except it pushes
+// its messages are sent to a delayExchange which keeps them there for delayMS time and then redirects
+// them to the conf.QueueName so consumers can start consuming them
+func NewDelayedPublisher(conf Config, delayExchange string, delayMS int) (Publisher, error) {
+	c := NewClient(conf).(*client)
+	return &delayedPublisher{
+		c,
+		delayExchange,
+		delayMS,
+	}, nil
+}
+
+func (p *delayedPublisher) Publish(j Job) error {
+	_, err := p.Bind()
+	if err != nil {
+		return err
+	}
+	defer p.conn.Close()
+
+	ch, err := p.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	// Create a delayed exchange
+	err = ch.ExchangeDeclare(
+		p.delayExchange,
+		"x-delayed-message",
+		true,
+		false,
+		false,
+		true,
+		amqp.Table{
+			"x-delayed-type": "direct",
+		},
+	)
+
+	if err != nil {
+		return err
+	}
+
+	q, err := ch.QueueDeclare(
+		p.QueueName(), // name
+		true,          // durable
+		false,         // delete when unused
+		false,         // exclusive
+		false,         // no-wait
+		nil,           // arguments
+	)
+	if err != nil {
+		return err
+	}
+
+	err = ch.QueueBind(
+		q.Name,          // queue name
+		p.QueueName(),   // routing key
+		p.delayExchange, // exchange
+		false,
+		nil)
+
+	if err != nil {
+		return err
+	}
+
+	bodyBytes, err := json.Marshal(j)
+	if err != nil {
+		return err
+	}
+	err = ch.Publish(
+		p.delayExchange, // exchange
+		p.QueueName(),   // routing key
+		false,           // mandatory
+		false,           // immediate
+		amqp.Publishing{
+			Headers: amqp.Table{
+				"x-delay": strconv.Itoa(p.delayMS),
+			},
+			ContentType: "application/json",
+			Body:        bodyBytes,
+		})
+
 	if err != nil {
 		return err
 	}
